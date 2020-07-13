dfe6dc8b76707711223ead9f7daaa03b58f2908f
sw1tchbl4d3 <jsimeze@pm.me>
2020-07-13 18:46:27 +0000

    First Version of the bot.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # ecsbot
-The bot for my discord server (Ethical Computing Society)
+The bot for my Discord server [Ethical Computing Society](https://discord.gg/CkMmZsR)
+
+## Functionality
+- Gives out role if rules have been accepted (react with a checkmark on RULESMSG in RULESCHANNEL)
+- Gives out custom roles (react with specified emoji on ROLESMSG in ROLESCHANNEL)
+- Reminds Users to accept the rules after 24,48 hours (Embed DM).
+- Kicks Users after not accepting the rules after 72 hours.
---
+++ config.py
@@ -0,0 +1,30 @@
+import discord
+TOKEN = "INSERT-TOKEN-HERE"
+PREFIX = "!"
+
+# ID of the Guild.
+GUILD = 690212435306741901
+
+# IDs of various channels.
+RULESCHANNEL = 690216616163672136
+RULESMSG = 690217807748530276
+ROLESCHANNEL = 690219084469895225
+ROLESMSG = 724236686971764767
+
+# base64 encoded emoji as reaction for accepting rules.
+RULESREACTION = b"4pyF"
+
+# Roles for the Rules-Mechanism.
+UNVERIFIEDROLE = 732212270385332225
+VERIFIEDROLE = 690212961876312094
+
+# Custom roles {b64reaction: [rolename, roleid]}
+ROLES = {b"8J+UlA==": ["ping", 690239204667818102], 
+         b"4oyo77iP": ["coder", 690243098533822485], 
+         b"8J+Suw==": ["hacker", 690243125225980007],  
+         b"8J+Upw==": ["hardware", 726809084975906856]}
+
+# Discord Embeds for Reminders
+WELCOMEMSG = discord.Embed(title="Ethical Computing Society", description="Welcome to the Ethical Computing Society! To be able to chat and accept the rules, react with a checkmark in #rules! If you have problems verifying, contact us in #verification-help!", color=0x57008b)
+REMINDERMSG = discord.Embed(title="Ethical Computing Society", description="We've seen that you didn't accept the rules yet! To accept them, react with a checkmark in #rules! If you have problems verifying, contact us in #verification-help!", color=0x57008b)
+KICKMSG = discord.Embed(title="Ethical Computing Society", description="We're sorry, but you didn't accept the rules and we had to kick you. You can always rejoin if you want to come back!", color=0x57008b)
