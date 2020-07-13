54473c47adadd246bc6ac755974ea1e58c7f7fb1
sw1tchbl4d3 <jsimeze@pm.me>
2020-07-13 19:05:45 +0000

    PEP8

--- app.py
+++ app.py
@@ -9,22 +9,27 @@ client = discord.Client()
 
 guild = None
 
+
 @client.event
 async def on_ready():
     global guild
     print("Bot is ready, " + client.user.name)
     guild = client.get_guild(GUILD)
-    await client.change_presence(status=discord.Status.online, activity=discord.Game("Ethical Computing Society!"))
+    await client.change_presence(status=discord.Status.online,
+                                 activity=discord.Game(STATUS))
     reminder.start()
+
 
 def write_reminds(reminds):
     with open('reminds.json', 'w') as json_file:
         json.dump({"reminds": reminds}, json_file)
 
+
 def read_reminds():
     with open('reminds.json', 'r') as json_file:
         reminds = json.load(json_file)
     return reminds["reminds"]
+
 
 @tasks.loop(minutes=1)
 async def reminder():
@@ -38,13 +43,16 @@ async def reminder():
             if i[6] in [0, 1]:
                 print("Reminded " + str(i[5]) + " " + str(i[6]))
                 newremind = reminder + timedelta(hours=24)
-                endreminds.append([newremind.year, newremind.month, newremind.day, newremind.hour, newremind.minute, i[5], i[6] + 1])
+                endreminds.append([newremind.year, newremind.month,
+                                   newremind.day, newremind.hour,
+                                   newremind.minute, i[5], i[6] + 1])
             else:
                 print("Kicked " + str(i[5]) + " " + str(i[6]))
-                guild.get_member(i[5]).kick(reason="Didn't accept the rules in time.")
+                guild.get_member(i[5]).kick(reason="Didn't accept the rules.")
         else:
             endreminds.append(i)
     write_reminds(endreminds)
+
 
 @client.event
 async def on_message(message):
@@ -56,40 +64,52 @@ async def on_message(message):
         command = args[0]
         args.pop(0)
 
+
 @client.event
 async def on_raw_reaction_add(payload):
     member = guild.get_member(payload.user_id)
     reaction = b64encode(str(payload.emoji).encode())
-    
-    if RULESMSG == payload.message_id and RULESCHANNEL == payload.channel_id and RULESREACTION == reaction:
-        await member.add_roles(guild.get_role(VERIFIEDROLE), reason="Accepted the Rules!")
-        await member.remove_roles(guild.get_role(UNVERIFIEDROLE), reason="Accepted the Rules!")
-        reminds = read_reminds()
-        endreminds = []
-        for i in reminds:
-            if i[5] != payload.user_id:
-                enreminds.append(i)
-        write_reminds(endreminds)
 
-    if ROLESMSG == payload.message_id and ROLESCHANNEL == payload.channel_id and reaction in ROLES.keys():
-        await member.add_roles(guild.get_role(ROLES[reaction][1]), reason="Added " + ROLES[reaction][0] + " role.")
+    if RULESMSG == payload.message_id and RULESCHANNEL == payload.channel_id:
+        if RULESREACTION == reaction:
+            await member.add_roles(guild.get_role(VERIFIEDROLE),
+                                   reason="Accepted the Rules!")
+            await member.remove_roles(guild.get_role(UNVERIFIEDROLE),
+                                      reason="Accepted the Rules!")
+            reminds = read_reminds()
+            endreminds = []
+            for i in reminds:
+                if i[5] != payload.user_id:
+                    endreminds.append(i)
+            write_reminds(endreminds)
+
+    if ROLESMSG == payload.message_id and ROLESCHANNEL == payload.channel_id:
+        if reaction in ROLES.keys():
+            await member.add_roles(guild.get_role(ROLES[reaction][1]),
+                                   reason="Added " + ROLES[reaction][0])
     return
+
 
 @client.event
 async def on_raw_reaction_remove(payload):
     member = guild.get_member(payload.user_id)
     reaction = b64encode(str(payload.emoji).encode())
 
-    if ROLESMSG == payload.message_id and ROLESCHANNEL == payload.channel_id and reaction in ROLES.keys():
-        await member.remove_roles(guild.get_role(ROLES[reaction][1]), reason="Removed " + ROLES[reaction][0] + " role.")
+    if ROLESMSG == payload.message_id and ROLESCHANNEL == payload.channel_id:
+        if reaction in ROLES.keys():
+            await member.remove_roles(guild.get_role(ROLES[reaction][1]),
+                                      reason="Removed " + ROLES[reaction][0])
     return
+
 
 @client.event
 async def on_member_join(member):
     reminds = read_reminds()
     reminder = datetime.now() + timedelta(hours=24)
-    reminds.append([reminder.year, reminder.month, reminder.day, reminder.hour, reminder.minute, member.id, 0])
+    reminds.append([reminder.year, reminder.month, reminder.day,
+                    reminder.hour, reminder.minute, member.id, 0])
     write_reminds(reminds)
     await member.send(embed=WELCOMEMSG)
+
 
 client.run(TOKEN)
--- config.py
+++ config.py
@@ -2,6 +2,8 @@ import discord
 TOKEN = "INSERT-TOKEN-HERE"
 PREFIX = "!"
 
+STATUS = "Ethical Computing Society!"
+
 # ID of the Guild.
 GUILD = 690212435306741901
 
@@ -19,12 +21,15 @@ UNVERIFIEDROLE = 732212270385332225
 VERIFIEDROLE = 690212961876312094
 
 # Custom roles {b64reaction: [rolename, roleid]}
-ROLES = {b"8J+UlA==": ["ping", 690239204667818102], 
-         b"4oyo77iP": ["coder", 690243098533822485], 
-         b"8J+Suw==": ["hacker", 690243125225980007],  
+ROLES = {b"8J+UlA==": ["ping", 690239204667818102],
+         b"4oyo77iP": ["coder", 690243098533822485],
+         b"8J+Suw==": ["hacker", 690243125225980007],
          b"8J+Upw==": ["hardware", 726809084975906856]}
 
 # Discord Embeds for Reminders
-WELCOMEMSG = discord.Embed(title="Ethical Computing Society", description="Welcome to the Ethical Computing Society! To be able to chat and accept the rules, react with a checkmark in #rules! If you have problems verifying, contact us in #verification-help!", color=0x57008b)
-REMINDERMSG = discord.Embed(title="Ethical Computing Society", description="We've seen that you didn't accept the rules yet! To accept them, react with a checkmark in #rules! If you have problems verifying, contact us in #verification-help!", color=0x57008b)
-KICKMSG = discord.Embed(title="Ethical Computing Society", description="We're sorry, but you didn't accept the rules and we had to kick you. You can always rejoin if you want to come back!", color=0x57008b)
+WELCOMEMSG = discord.Embed(title="Ethical Computing Society", 
+                           description="Welcome to the Ethical Computing Society! To be able to chat and accept the rules, react with a checkmark in #rules! If you have problems verifying, contact us in #verification-help!", color=0x57008b)
+REMINDERMSG = discord.Embed(title="Ethical Computing Society", 
+                           description="We've seen that you didn't accept the rules yet! To accept them, react with a checkmark in #rules! If you have problems verifying, contact us in #verification-help!", color=0x57008b)
+KICKMSG = discord.Embed(title="Ethical Computing Society", 
+                           description="We're sorry, but you didn't accept the rules and we had to kick you. You can always rejoin if you want to come back!", color=0x57008b)
